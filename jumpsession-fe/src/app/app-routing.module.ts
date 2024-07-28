d44a679718139273999492acf1cdb8cc767ba5b4
CarlWehl <122958739+CarlWehl@users.noreply.github.com>
2024-07-28 11:49:53 +0200

    Routing

--- jumpsession-fe/src/app/app-routing.module.ts
+++ jumpsession-fe/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
