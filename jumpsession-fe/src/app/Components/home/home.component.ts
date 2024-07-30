88b4f6caf1c54205d9a0adb8aa9edcde5a803b2f
CarlWehl <122958739+CarlWehl@users.noreply.github.com>
2024-07-30 21:21:22 +0200

    Added searchable table to home

--- jumpsession-fe/src/app/Components/home/home.component.ts
+++ jumpsession-fe/src/app/Components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Profile } from 'src/app/Models/profile';
+import { ProfileService } from 'src/app/Services/profile.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  profiles: Profile[] = [];
+  filteredUsers: Profile[] = [];
+  searchTerm: string = '';
+  errorMessage: string = '';
 
+  constructor(private profileService: ProfileService) { }
+
+  ngOnInit(): void {
+    this.profileService.getUsers().subscribe(
+      (data: Profile[]) => {
+        this.profiles = data;
+        this.filteredUsers = this.profiles;
+      },
+      error => {
+        this.errorMessage = 'An error occurred while fetching the user list';
+      }
+    );
+  }
+
+  filterUsers() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredUsers = this.profiles.filter(profile =>
+      profile.name.toLowerCase().includes(term) ||
+      profile.surname.toLowerCase().includes(term) ||
+      profile.email.toLowerCase().includes(term) ||
+      profile.phoneNumber.toLowerCase().includes(term)
+    );
+  }
 }
