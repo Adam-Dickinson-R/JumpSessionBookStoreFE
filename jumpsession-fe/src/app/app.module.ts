af6f2f52d6956cf4c9c7e1ffdf7806edc808379a
Matthew <mschoeman@retrorabbit.co.za>
2024-08-06 22:35:12 +0200

    added nav service

--- jumpsession-fe/src/app/app.module.ts
+++ jumpsession-fe/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { UsersComponent } from './Components/users/users.component';
+import { BooksComponent } from './Components/books/books.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { AssignComponent } from './Components/assign/assign.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    UsersComponent,
+    BooksComponent,
+    NavbarComponent,
+    AssignComponent
   ],
   imports: [
     BrowserModule,
